cfc9473777383a8665216c2796e9aed9abc7e51c
JinHojun <silvergold9@naver.com>
2021-03-31 16:00:57 +0900

    플레이스토어 등록을 위한 패키지명 수정

---
+++ app/src/main/java/com/MakaoTalk/makaotalk/StartServiceActivity.java
@@ -0,0 +1,162 @@
+package com.MakaoTalk.makaotalk;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.MakaoTalk.makaotalk.wifiscan.PreferenceManager;
+
+public class StartServiceActivity extends AppCompatActivity {
+
+    public static Switch switch1;
+    public static TextView tv_status;
+
+     public static FrameLayout line1, line2;
+
+    public static Intent foregroundServiceIntent;
+
+    private TextView tv_wifi;
+    public static boolean checkSwitch;
+
+    private Context mContext;
+    public String wifi_saved;
+
+    ImageView ImageView_settings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startservice);
+        this.getIntent();
+
+        mContext = this.getApplicationContext();
+
+        tv_wifi = findViewById(R.id.tv_svWifiName);
+        tv_status = findViewById(R.id.tv_status);
+
+        line1 = findViewById(R.id.line1);
+        line2 = findViewById(R.id.line2);
+
+        //와이파이 강도 측정 기능 제어할 스위치 삽입
+        switch1 = findViewById(R.id.switch1);
+
+        //설정 버튼 삽입
+       ImageView_settings = findViewById(R.id.ImageView_settings);
+
+        //설정 버튼 클릭하면 settings_popup으로 화면 이동
+       ImageView_settings.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent intent = new Intent(getApplicationContext(), SettingsPopup.class);
+               startActivity(intent);
+           }
+       });
+
+        //이미 foreground service가 작동중인 상태에서 앱을 재실행한 상태라면
+        if (null != UndeadService.serviceIntent){
+            foregroundServiceIntent = UndeadService.serviceIntent;
+            //Toast.makeText(getApplicationContext(), "already", Toast.LENGTH_SHORT).show();
+        }
+
+
+        wLoadFile();      //텍스트뷰에 등록된 와이파이 표시
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.switch1:
+                        if(switch1.isChecked()){
+                            //foreground service 시작
+                            checkSwitch = true;
+                            startForeground();
+
+                            tv_status.setText("ON");
+                            //tv_status.setTextColor(Color.parseColor("#db7c4d"));
+                            line1.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.custom_line_on1));
+                            line2.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.custom_line_on2));
+                            onResume();
+                        }else {
+                            //foreground service 종료
+                            if (null != foregroundServiceIntent) {
+                                Log.d("system", "foreground service 종료");
+                                stopService(foregroundServiceIntent);
+                                foregroundServiceIntent = null;
+                                UndeadService.serviceIntent = null;
+                                UndeadService.am.cancel(UndeadService.sender);
+                                checkSwitch = false;
+
+                                tv_status.setText("OFF");
+                                //tv_status.setTextColor(Color.parseColor("#4e7da6"));
+                                line1.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.custom_line));
+                                line2.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.custom_line2));
+                                onResume();
+                            }
+                        }
+                }
+            }
+        };
+        switch1.setOnClickListener(listener);
+
+
+    }
+
+
+    //WIFI FILE 불러오기
+    public void wLoadFile(){
+        wifi_saved = PreferenceManager.getString(mContext,"Wifi_ssid");
+        tv_wifi.setText(wifi_saved);
+    }
+
+    public void startForeground(){
+        if (null == UndeadService.serviceIntent) {
+            foregroundServiceIntent = new Intent(this, UndeadService.class);
+            startService(foregroundServiceIntent);
+            //Toast.makeText(getApplicationContext(), "start service", Toast.LENGTH_SHORT).show();
+        } else {
+            foregroundServiceIntent = UndeadService.serviceIntent;
+            //Toast.makeText(getApplicationContext(), "already", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(checkSwitch){
+            switch1.setChecked(true); //앱 종료해도 스위치 상태 고정
+            tv_status.setText("ON");
+            //tv_status.setTextColor(Color.parseColor("#db7c4d"));
+            line1.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.custom_line_on1));
+            line2.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.custom_line_on2));
+        }else{
+            tv_status.setText("OFF");
+            //tv_status.setTextColor(Color.parseColor("#4e7da6"));
+            line1.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.custom_line));
+            line2.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.custom_line2));
+        }
+    }
+
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (null != foregroundServiceIntent) {
+            stopService(foregroundServiceIntent);
+            foregroundServiceIntent = null;
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/MakaoTalk/makaotalk/popup/TensorFlowImageClassifier.java
@@ -0,0 +1,199 @@
+package com.MakaoTalk.makaotalk.popup;
+
+import android.annotation.SuppressLint;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+
+import org.tensorflow.lite.Interpreter;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class TensorFlowImageClassifier implements Classifier {
+
+    private static final int MAX_RESULTS = 3;
+    private static final int BATCH_SIZE = 1;
+    private static final int PIXEL_SIZE = 3;
+    private static final float THRESHOLD = 0.1f;
+
+    private static final int IMAGE_MEAN = 128;
+    private static final float IMAGE_STD = 128.0f;
+
+    private Interpreter interpreter;
+    private int inputSize;
+    private List<String> labelList;
+    private boolean quant;
+
+    private TensorFlowImageClassifier() {
+
+    }
+
+    static Classifier create(AssetManager assetManager, String modelPath, String labelPath, int inputSize, boolean quant) throws IOException {
+        TensorFlowImageClassifier classifier = new TensorFlowImageClassifier();
+// interpreter 생성자 (MappedByteBuffer(모델이매핑된) , 인터프리터옵션?)
+        classifier.interpreter = new Interpreter(classifier.loadModelFile(assetManager, modelPath), new Interpreter.Options());
+        classifier.labelList = classifier.loadLabelList(assetManager, labelPath);
+        classifier.inputSize = inputSize;
+        classifier.quant = quant;
+
+        return classifier;
+    }
+
+    @Override
+    public List<Recognition> recognizeImage(Bitmap bitmap) {
+        ByteBuffer byteBuffer = convertBitmapToByteBuffer(bitmap);
+        if (quant) {
+            // result 배열은
+            byte[][] result = new byte[1][labelList.size()];
+            //비트맵이미지, labelList크기의 byte 배열
+            interpreter.run(byteBuffer, result);
+            return getSortedResultByte(result);
+        } else {
+            float[][] result = new float[1][labelList.size()];
+            interpreter.run(byteBuffer, result);
+            return getSortedResultFloat(result);
+        }
+    }
+
+    @Override
+    public void close() {
+        interpreter.close();
+        interpreter = null;
+    }
+
+    // mobilenet_quant_v1_224.tflite 경로를 받아와서
+//
+    private MappedByteBuffer loadModelFile(AssetManager assetManager, String modelPath) throws IOException {
+        // AssetManager.openFd
+        // Open an uncompressed asset by mmapping it and returning an AssetFileDescriptor.
+        // 압축되지 않은 자산을 mmapping하고 AssetFileDescriptor를 반환하여 엽니다.
+        AssetFileDescriptor fileDescriptor = assetManager.openFd(modelPath);
+
+        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+        long startOffset = fileDescriptor.getStartOffset();
+        long declaredLength = fileDescriptor.getDeclaredLength();
+        // 이 채널의 파일 영역을 메모리에 직접 매핑
+        // map(FileChannel.MapMode mode, long position, long size)
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+        // MappedByteBuffer리턴
+    }
+
+    // label.txt 경로를 받아와서 labelList 리스트에 한줄씩 저장
+    private List<String> loadLabelList(AssetManager assetManager, String labelPath) throws IOException {
+        List<String> labelList = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(assetManager.open(labelPath)));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            labelList.add(line);
+        }
+        reader.close();
+        return labelList;
+    }
+
+    private ByteBuffer convertBitmapToByteBuffer(Bitmap bitmap) {
+        ByteBuffer byteBuffer;
+
+        if (quant) {
+            byteBuffer = ByteBuffer.allocateDirect(BATCH_SIZE * inputSize * inputSize * PIXEL_SIZE);
+        } else {
+            byteBuffer = ByteBuffer.allocateDirect(4 * BATCH_SIZE * inputSize * inputSize * PIXEL_SIZE);
+        }
+
+        byteBuffer.order(ByteOrder.nativeOrder());
+        int[] intValues = new int[inputSize * inputSize];
+        bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+        int pixel = 0;
+        for (int i = 0; i < inputSize; ++i) {
+            for (int j = 0; j < inputSize; ++j) {
+                final int val = intValues[pixel++];
+                if (quant) {
+                    byteBuffer.put((byte) ((val >> 16) & 0xFF));
+                    byteBuffer.put((byte) ((val >> 8) & 0xFF));
+                    byteBuffer.put((byte) (val & 0xFF));
+                } else {
+                    byteBuffer.putFloat((((val >> 16) & 0xFF) - IMAGE_MEAN) / IMAGE_STD);
+                    byteBuffer.putFloat((((val >> 8) & 0xFF) - IMAGE_MEAN) / IMAGE_STD);
+                    byteBuffer.putFloat((((val) & 0xFF) - IMAGE_MEAN) / IMAGE_STD);
+                }
+
+            }
+        }
+        return byteBuffer;
+    }
+
+    @SuppressLint("DefaultLocale")
+    private List<Recognition> getSortedResultByte(byte[][] labelProbArray) {
+
+        PriorityQueue<Recognition> pq =
+                new PriorityQueue<>(
+                        MAX_RESULTS,
+                        new Comparator<Recognition>() {
+                            @Override
+                            public int compare(Recognition lhs, Recognition rhs) {
+                                return Float.compare(rhs.getConfidence(), lhs.getConfidence());
+                            }
+                        });
+
+        for (int i = 0; i < labelList.size(); ++i) {
+            float confidence = (labelProbArray[0][i] & 0xff) / 255.0f;
+            // 확률이 0.1보다 크면
+            if (confidence > THRESHOLD) {
+                pq.add(new Recognition("" + i,
+                        labelList.size() > i ? labelList.get(i) : "unknown",
+                        confidence, quant));
+            }
+        }
+
+        final ArrayList<Recognition> recognitions = new ArrayList<>();
+        // math.min() : 작은 값을 리턴
+        int recognitionsSize = Math.min(pq.size(), MAX_RESULTS);
+        for (int i = 0; i < recognitionsSize; ++i) {
+            recognitions.add(pq.poll());
+        }
+
+        return recognitions;
+    }
+
+    @SuppressLint("DefaultLocale")
+    private List<Recognition> getSortedResultFloat(float[][] labelProbArray) {
+
+        PriorityQueue<Recognition> pq =
+                new PriorityQueue<>(
+                        MAX_RESULTS,
+                        new Comparator<Recognition>() {
+                            @Override
+                            public int compare(Recognition lhs, Recognition rhs) {
+                                return Float.compare(rhs.getConfidence(), lhs.getConfidence());
+                            }
+                        });
+
+        for (int i = 0; i < labelList.size(); ++i) {
+            float confidence = labelProbArray[0][i];
+            if (confidence > THRESHOLD) {
+                pq.add(new Recognition("" + i,
+                        labelList.size() > i ? labelList.get(i) : "unknown",
+                        confidence, quant));
+            }
+        }
+
+        final ArrayList<Recognition> recognitions = new ArrayList<>();
+        int recognitionsSize = Math.min(pq.size(), MAX_RESULTS);
+        for (int i = 0; i < recognitionsSize; ++i) {
+            recognitions.add(pq.poll());
+        }
+
+        return recognitions;
+    }
+}
